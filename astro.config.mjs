d0ffbd39e8ca0ba9c1c7262399a2e19f9e2cb2bc
0x4f48 <0x4f48@proton.me>
2024-04-29 21:59:05 +0200

    header, tailwindify, font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), react(), tailwind({
+    applyBaseStyles: false,
+  })],
   output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true
+    }
   })
 });
