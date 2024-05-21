d975e22c98317ac16bc4a7c1c2d4e043964fad47
4f48 <0x4f48@proton.me>
2024-05-21 22:09:16 +0200

    Decided to use Drizzle instead

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+import "@/drizzle/env";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: './src/drizzle/schema.ts',
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL,
+  },
+});
