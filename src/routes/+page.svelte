9be7a1130e34e0f451f8055f3fd3681bc3370577
4f48 <0x4f48@proton.me>
2024-06-23 11:03:55 +0200

    Move publish dialog out from player component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import Player from "../components/player.svelte";
+
+	import * as Dialog from "$lib/components/ui/dialog";
+	import * as AlertDialog from "$lib/components/ui/alert-dialog";
+	import { Input } from "$lib/components/ui/input/index.js";
+	import Button from "@/components/ui/button/button.svelte";
+
+	import { openPublish } from "@/stores";
+	let name: string;
 </script>
 
 <svelte:head>
@@ -11,3 +19,35 @@
 </svelte:head>
 
 <Player />
+
+<Dialog.Root bind:open={$openPublish}>
+	<!-- Remove later -->
+	<Dialog.Trigger>DB</Dialog.Trigger>
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title class="flex items-center gap-2">You are a GENIUSZ!</Dialog.Title>
+			<Dialog.Description>You may now publish your game to the leaderboard.</Dialog.Description>
+		</Dialog.Header>
+		<Input type="text" id="name" placeholder="Username" bind:value={name} />
+
+		<Dialog.Footer>
+			<AlertDialog.Root>
+				<AlertDialog.Trigger>
+					<Button variant="destructive">Don't publish</Button>
+				</AlertDialog.Trigger>
+				<AlertDialog.Content>
+					<AlertDialog.Header>
+						<AlertDialog.Title>Are you sure?</AlertDialog.Title>
+						<AlertDialog.Description>This action cannot be undone.</AlertDialog.Description>
+					</AlertDialog.Header>
+					<AlertDialog.Footer>
+						<AlertDialog.Action>Take me back</AlertDialog.Action>
+						<AlertDialog.Cancel on:click={() => ($openPublish = false)}>I'm sure</AlertDialog.Cancel
+						>
+					</AlertDialog.Footer>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
+			<Button disabled>Submit</Button>
+		</Dialog.Footer>
+	</Dialog.Content>
+</Dialog.Root>
