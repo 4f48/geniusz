33da5d7e803f956db9cde108dc1081904ee9191c
4f48 <0x4f48@proton.me>
2024-06-23 10:33:49 +0200

    Run svelte-add on that to add Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const { POSTGRES_URL } = process.env;
-if (!POSTGRES_URL) {
-	throw new Error("No POSTGRES_URL was provided.");
-}
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 export default defineConfig({
+	schema: "./src/lib/server/db/schema.ts",
 	dialect: "postgresql",
-	schema: "./src/lib/schema.ts",
-	out: "./drizzle",
-	dbCredentials: {
-		url: process.env.POSTGRES_URL!
-	}
-});
 
+	dbCredentials: {
+		url: process.env.DATABASE_URL
+	},
+
+	verbose: true,
+	strict: true
+});
