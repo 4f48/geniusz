bdcc468d70c4545d9e436296a44e09e5677f13f3
4f48 <0x4f48@proton.me>
2024-06-24 13:11:33 +0200

    Add a screen for empty DB

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -20,34 +20,41 @@
 </svelte:head>
 
 <div class="mt-12 flex h-full w-full justify-center">
-	<div class="w-[50%] p-5">
-		<Table.Root>
-			<Table.Header>
-				<Table.Row>
-					<Table.Head>Player</Table.Head>
-					<Table.Head>Time</Table.Head>
-					<Table.Head>Moves</Table.Head>
-					<Table.Head class="text-right">Published at</Table.Head>
-				</Table.Row>
-			</Table.Header>
-			<Table.Body>
-				{#each data.result as entry}
+	{#if data.result.length > 0}
+		<div class="w-[50%] p-5">
+			<Table.Root>
+				<Table.Header>
 					<Table.Row>
-						<Table.Cell>{entry.player}</Table.Cell>
-						<Table.Cell class="font-mono"
-							>{`${Math.floor(entry.time / 60000) < 10 ? 0 + Math.floor(entry.time / 60000).toString() : Math.floor(entry.time / 60000)}:${Math.floor((entry.time % 60000) / 1000) < 10 ? 0 + Math.floor((entry.time % 60000) / 1000).toString() : Math.floor((entry.time % 60000) / 1000)}:${entry.time % 1000 < 10 ? 0 + (entry.time % 1000).toString() : entry.time % 1000}`}</Table.Cell
-						>
-						<Table.Cell
-							><a href={download(JSON.stringify(entry.moves))} download="moves"
-								><Button variant="secondary" size="sm" class="flex items-center gap-2"
-									><Download size="16px" /> Download</Button
-								></a
-							></Table.Cell
-						>
-						<Table.Cell class="text-right">{entry.date.toLocaleDateString()}</Table.Cell>
+						<Table.Head>Player</Table.Head>
+						<Table.Head>Time</Table.Head>
+						<Table.Head>Moves</Table.Head>
+						<Table.Head class="text-right">Published at</Table.Head>
 					</Table.Row>
-				{/each}
-			</Table.Body>
-		</Table.Root>
-	</div>
+				</Table.Header>
+				<Table.Body>
+					{#each data.result as entry}
+						<Table.Row>
+							<Table.Cell>{entry.player}</Table.Cell>
+							<Table.Cell class="font-mono"
+								>{`${Math.floor(entry.time / 60000) < 10 ? 0 + Math.floor(entry.time / 60000).toString() : Math.floor(entry.time / 60000)}:${Math.floor((entry.time % 60000) / 1000) < 10 ? 0 + Math.floor((entry.time % 60000) / 1000).toString() : Math.floor((entry.time % 60000) / 1000)}:${entry.time % 1000 < 10 ? 0 + (entry.time % 1000).toString() : entry.time % 1000}`}</Table.Cell
+							>
+							<Table.Cell
+								><a href={download(JSON.stringify(entry.moves))} download="moves"
+									><Button variant="secondary" size="sm" class="flex items-center gap-2"
+										><Download size="16px" /> Download</Button
+									></a
+								></Table.Cell
+							>
+							<Table.Cell class="text-right">{entry.date.toLocaleDateString()}</Table.Cell>
+						</Table.Row>
+					{/each}
+				</Table.Body>
+			</Table.Root>
+		</div>
+	{:else}
+		<div class="flex w-[30%] flex-col items-center gap-1">
+			<h1 class="text-xl font-bold">No one managed to become a Geniusz yet.</h1>
+			<a class="w-full" href="/"><Button class="w-full">Be the first. Play now!</Button></a>
+		</div>
+	{/if}
 </div>
