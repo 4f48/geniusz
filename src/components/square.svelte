d38d5483fd4c21661e5a517d5b8b90b018720072
4f48 <0x4f48@proton.me>
2024-07-06 17:48:41 +0200

    Fix layout shift on mobile

--- src/components/square.svelte
+++ src/components/square.svelte
@@ -3,29 +3,29 @@
 </script>
 
 {#if type == "invisible"}
-	<div class="m-1 h-[12vw] w-[12vw] md:h-[6vw] md:w-[6vw]"></div>
+	<div class="m-1 h-[11vw] w-[11vw] md:h-[5.5vw] md:w-[5.5vw]"></div>
 {:else}
 	<div
-		class="group m-1 flex h-[12vw] w-[12vw] cursor-pointer items-center justify-center rounded-lg border border-foreground md:h-[6vw] md:w-[6vw]"
+		class="group m-1 flex h-[11vw] w-[11vw] cursor-pointer items-center justify-center rounded-lg border border-foreground md:h-[5.5vw] md:w-[5.5vw]"
 	>
 		{#if type == "occupied"}
 			<div
-				class="h-[6vw] w-[6vw] rounded-full bg-foreground duration-300 group-hover:bg-muted-foreground md:h-[3vw] md:w-[3vw]"
+				class="h-[5.5vw] w-[5.5vw] rounded-full bg-foreground duration-300 group-hover:bg-muted-foreground md:h-[3vw] md:w-[3vw]"
 			></div>
 		{/if}
 		{#if type == "selected"}
 			<div
-				class="h-[6vw] w-[6vw] rounded-full bg-[hsl(183,33%,40%)] duration-300 group-hover:bg-[hsl(190,89%,25%)] dark:bg-[hsl(157,16%,58%)] dark:group-hover:bg-[hsl(183,33%,40%)] md:h-[3vw] md:w-[3vw]"
+				class="h-[5.5vw] w-[5.5vw] rounded-full bg-[hsl(183,33%,40%)] duration-300 group-hover:bg-[hsl(190,89%,25%)] dark:bg-[hsl(157,16%,58%)] dark:group-hover:bg-[hsl(183,33%,40%)] md:h-[3vw] md:w-[3vw]"
 			></div>
 		{/if}
 		{#if type == "destination"}
 			<div
-				class="h-[6vw] w-[6vw] rounded-full bg-[hsl(122,21%,51%)] duration-300 group-hover:bg-[hsl(143,30%,37%)] dark:bg-[hsl(104,35%,62%)] dark:group-hover:bg-[hsl(122,21%,51%)] md:h-[3vw] md:w-[3vw]"
+				class="h-[5.5vw] w-[5.5vw] rounded-full bg-[hsl(122,21%,51%)] duration-300 group-hover:bg-[hsl(143,30%,37%)] dark:bg-[hsl(104,35%,62%)] dark:group-hover:bg-[hsl(122,21%,51%)] md:h-[3vw] md:w-[3vw]"
 			></div>
 		{/if}
 		{#if type == "unoccupied"}
 			<div
-				class="h-[6vw] w-[6vw] rounded-full border border-foreground duration-300 group-hover:bg-secondary md:h-[3vw] md:w-[3vw]"
+				class="h-[5.5vw] w-[5.5vw] rounded-full border border-foreground duration-300 group-hover:bg-secondary md:h-[3vw] md:w-[3vw]"
 			></div>
 		{/if}
 	</div>
